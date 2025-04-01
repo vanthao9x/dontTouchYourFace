b46988205fb0ceb5a0ac3da3a2940d85e140a80e
vanthao9x <levanthaolvt228@gmail.com>
2025-04-01 13:14:39 +0700

    finish training

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import "./App.css";
-import React, {useEffect, useRef} from "react";
-// import { Howl } from 'howler';
-// import soundUrl from './assets/hey_sondn.mp3';
+import React, { useEffect, useRef, useState } from "react";
+import { initNotifications, notify } from '@mycv/f8-notification';
+import { Howl } from 'howler';
+import soundUrl from './assets/hey_sondn.mp3';
 import * as tf from "@tensorflow/tfjs";
 import * as mobilenet from "@tensorflow-models/mobilenet";
 import * as knnClassifier from "@tensorflow-models/knn-classifier";
 
+var sound = new Howl({
+  src: [soundUrl]
+});
 
-// var sound = new Howl({
-//   src: [soundUrl]
-// });
-
-// sound.play();
-
+const NOT_TOUCH_LABEL = "not_touch_your_face";
+const TOUCHED_LABEL = "touched_your_face";
+const TRAINNING_TIME = 50;
+const TOUCHED_CONFIDENCE = 0.8;
 function App() {
-
+  const [touched, setTouched] = useState(false);
+  const canPlaySound = useRef(true);
   const videoRef = useRef(null);
   const streamRef = useRef(null); // Để lưu stream camera và cleanup khi cần
-  const init = async () =>{
+  const classifier = useRef(null);
+  const mobilenetModule = useRef(null); //
+  const init = async () => {
     console.log("init...");
 
     await tf.setBackend("webgl"); // Chọn backend
@@ -28,13 +33,14 @@ function App() {
     console.log("set up camera success ...");
 
     // Create the classifier.
-    const classifier = knnClassifier.create();
-    const mobilenetModule = await mobilenet.load();
+    classifier.current = knnClassifier.create();
+    mobilenetModule.current = await mobilenet.load();
 
     console.log("set up all success ...");
     console.log("không chạm tay lên mặt và bấm train 1");
 
-  }
+    initNotifications({ cooldown: 3000 });
+  };
 
   const setUpCamera = async () => {
     try {
@@ -51,8 +57,76 @@ function App() {
     }
   };
 
+  const train = async (label) => {
+    console.log(label);
+    for (let i = 0; i < TRAINNING_TIME; i++) {
+      console.log(`Progress ${parseInt(((i + 1) / TRAINNING_TIME) * 100)}%`);
+
+      await trainning(label);
+    }
+    /**
+     * Train máy cho khuôn mặt không chạm tay
+     * bước 2 trainning máy cho khuôn mặt chạm tay
+     * bước 3 lấy hình ảnh hiện taij và so sánh với data đã học trước đó
+     */
+  };
+
+  const trainning = (label) => {
+    return new Promise(async (resolve) => {
+      const embedding = mobilenetModule.current.infer(videoRef.current, true);
+      classifier.current.addExample(embedding, label);
+
+      await sleep(1);
+      resolve();
+    });
+  };
+
+  /** trong function run
+   * classifier.predictClass(
+    input: tf.Tensor,
+    k = 3
+    ): Promise<{label: string, classIndex: number, confidences: {[classId: number]: number}}>;
+  */
+
+  const run = async () => {
+    const embedding = mobilenetModule.current.infer(videoRef.current, true);
+    const result = await classifier.current.predictClass(embedding);
+    console.log("Label: ",result.label);
+    console.log("Confidence: ",result.confidences);
+
+    if (
+      result.label === TOUCHED_LABEL &&
+      result.confidences[result.label] > TOUCHED_CONFIDENCE) 
+      {
+      console.log("Chạm tay lên mặt");
+      if (canPlaySound.current) {
+        canPlaySound.current = false;
+        sound.play();
+      }
+      notify('Bỏ tay ra', { body: 'Bạn vừa chạm tay vào mặt!' });
+      setTouched(true);
+    }
+    else {
+      console.log("Không chạm tay lên mặt");
+      setTouched(false);
+    }
+
+    await sleep(2);
+    run();
+  };
+
+  const sleep = (seconds = 0) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, seconds * 100);
+    });
+  };
+
   useEffect(() => {
     init();
+
+    sound.on('end', function(){
+      canPlaySound.current = true;
+    });
 
     // clean up
     return () => {
@@ -64,13 +138,34 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <video ref={videoRef} className="video" autoPlay/>
+    <div className={`App ${touched ? "touched" : "not-touched"}`}>
+      <video ref={videoRef} className="video" autoPlay />
 
       <div className="control">
-        <button className="button" onClick={() => console.log("Play")}>Training 1</button>
-        <button className="button" onClick={() => console.log("Pause")}>Training 2</button>
-        <button className="button" onClick={() => console.log("Stop")}>Stop</button>
+        <button        
+          className="button"
+          onClick={() => {
+            train(NOT_TOUCH_LABEL);
+          }}
+        >
+          Training 1
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            train(TOUCHED_LABEL);
+          }}
+        >
+          Training 2
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            run();
+          }}
+        >
+          Stop
+        </button>
       </div>
     </div>
   );
